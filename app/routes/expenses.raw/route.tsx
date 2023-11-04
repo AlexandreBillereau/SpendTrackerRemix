db1123b0fabf6754f351bdf525ca8db86d47a984
alxeandre <alexandre.mr.billerau@gmail.com>
2023-11-04 17:42:32 -0400

    use url parameters to change ui

--- app/routes/expenses.raw/route.tsx
+++ app/routes/expenses.raw/route.tsx
@@ -1,3 +1,19 @@
-export default function ExpensesRawPage() {
-  return <h1>expenses raw</h1>;
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "seconde expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+//triger with get request basisc usage.
+export function loader() {
+  return DUMMY_EXPENSES;
 }
