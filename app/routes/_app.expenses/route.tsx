02353ff254c3beedc828bded70fe19343cfdde32
alxeandre <alexandre.mr.billerau@gmail.com>
2023-11-05 10:25:11 -0500

    setup prisma

--- app/routes/_app.expenses/route.tsx
+++ app/routes/_app.expenses/route.tsx
@@ -1,5 +1,6 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
+import { FaPlus, FaDownload } from "react-icons/fa";
 
 const DUMMY_EXPENSES = [
   {
@@ -21,6 +22,16 @@ export default function Expenses() {
     <>
       <Outlet></Outlet>
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="expenses/raw">
+            <FaDownload />
+            <span>Add Expense</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES}></ExpensesList>
       </main>
     </>
