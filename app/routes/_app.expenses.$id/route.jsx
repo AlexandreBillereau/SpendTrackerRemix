1948e0184451abb6f7403c3f00c2512f7c233f35
alxeandre <alexandre.mr.billerau@gmail.com>
2023-11-07 12:13:24 -0500

    Feature : update expenses

--- app/routes/_app.expenses.$id/route.jsx
+++ app/routes/_app.expenses.$id/route.jsx
@@ -1,6 +1,9 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpensesForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 // import { getExpensesById } from "~/data/expenses.server";
 
 export default function UpdateExpensesPage() {
@@ -21,3 +24,22 @@ export default function UpdateExpensesPage() {
 //   const expenses = await getExpensesById(id);
 //   return expenses;
 // }
+
+/**
+ * @param {import("@remix-run/node").ActionFunctionArgs}
+ */
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  //dont forget validation
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect("..");
+}
