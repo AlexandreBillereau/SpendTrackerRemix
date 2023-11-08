eea796964b5401fce567ec4383b1898213cd152c
alxeandre <alexandre.mr.billerau@gmail.com>
2023-11-08 13:02:12 -0500

    handling error page

---
+++ app/root.jsx
@@ -0,0 +1,71 @@
+import {
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+} from "@remix-run/react";
+import sharedStyles from "~/styles/shared.css";
+import Error from "./components/util/Error";
+
+export const links = () => [{ rel: "stylesheet", href: sharedStyles }];
+
+function Document({ title, children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet></Outlet>
+    </Document>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Document title={error.statusText}>
+        <main>
+          <Error title={error.statusText}>
+            <p>
+              {error.data?.message ||
+                "Something went wrong, please try again later!"}
+            </p>
+            <p>
+              Back to <Link to={"/"}>safety</Link>.
+            </p>
+          </Error>
+        </main>
+      </Document>
+    );
+  }
+}
